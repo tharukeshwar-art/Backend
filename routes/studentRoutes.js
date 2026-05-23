9d7592abd2c884704b3f16d601f7b767bc4f9630
tharukeshwar-art <tharukeshwarsenthil@gmail.com>
2026-05-23 17:57:01 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -5,16 +5,16 @@ const router = express.Router();
 const studentController = require("../controllers/studentController");
 
 
-// GET
+
 router.get("/", studentController.getStudents);
 
-// POST
+
 router.post("/", studentController.addStudent);
 
-// PUT
+
 router.put("/:id", studentController.updateStudent);
 
-// DELETE
+
 router.delete("/:id", studentController.deleteStudent);
 
 
